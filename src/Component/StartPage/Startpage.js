07c914737fa8c0d324d12413bee8461312404af3
Yash Mukund Purohit <j.samkumar@shivila.com>
2022-01-22 20:00:16 +0530

    social handle added

--- src/Component/StartPage/Startpage.js
+++ src/Component/StartPage/Startpage.js
@@ -1,4 +1,5 @@
 import './start.css'
+import {BsFacebook ,BsYoutube ,BsGithub ,BsLinkedin} from 'react-icons/bs'
 
 export const Startpage = ({CardShuffle}) => {
     return (
@@ -8,9 +9,19 @@ export const Startpage = ({CardShuffle}) => {
                 <span>Card Molotov</span>
 
             </div>
+            <p>Score minimum </p>
             <button onClick={CardShuffle}>Start Game</button>
-          
-
+            
+         
+            <div className="socialMedia">
+                <p>follow me on :</p>
+                 <a href='https://www.linkedin.com/in/yash-purohit-a135321b1/'><BsLinkedin/></a>
+                 <a href='https://github.com/yashPurohit01'><BsGithub/></a>
+                 <a href='https://www.youtube.com/channel/UCynO2RXIN3nJHJYtWKcPStw'><BsYoutube/></a>
+                 <a href='https://www.facebook.com/fantastic.yash'><BsFacebook/></a>
+                 
+                 
+            </div>   
         </div>
     )
 }
